f52ce6821d1d8e26c6add847ff7c280819d1ed33
ThucNu <thucnu0608@gmail.com>
2022-09-23 12:30:21 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 
-const app = express();
+var express = require("express");
+// var bodyParser = require("body-parser");
+var app = express();
 
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+// app.use(bodyParser.json());
+
+app.use(cors());
+app.use(express.json());
 app.use("/api/contacts", contactRouter);
 // handle 404 response
 app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
   return res
     .status(error.statusCode || 500)
     .json({ message: error.message || "Internal Server Error" });
 });
-app.use(cors());
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
